942ac1d8b1ee2a3cf6b9e2704a40566dc412a730
michael121yang <michael.yang@tufts.edu>
2015-09-29 09:59:59 -0400

    Further formatting of player.js
    Should work as an object. Unsure if global variables are okay, will have
    to see.

--- js/player.js
+++ js/player.js
@@ -1,10 +1,9 @@
-//player.js
+//js
 //contains code for player actions and sprite
-var Bitshift = Bitshift || {};
+//requires preloading: this.game.load.spritesheet('dude','assets/dude.png',32,48);
 
-Bitshift.Player = function(){};
-
-Bitshift.Player.prototype = {
+player.prototype = Object.create(Phaser.Sprite.prototype);
+player.prototype.constructor = player;
 
 var DEFAULT_SPEED = 150;
 var JUMP_SPEED = 450;
@@ -14,96 +13,98 @@ var SIDE_DECAY_NUM = 5;
 var DOWN_SPEED = 400;
 var VTEST_SPEED = 300;
 var shifted = false;
+var cursors;
+var spacebar;
+var vkey;
+
+//Creates a player at x and y
+function player(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'dude');
+    game.camera.follow(this);
+    this.anchor.setTo(.5,.5);
     
-preload: function() {
-    this.game.load.spritesheet('dude','assets/dude.png',32,48);
+    game.physics.arcade.enable(this);
+    this.body.gravity.y  = 500;
+    this.body.collideWorldBounds = true;
+    
+    this.animations.add('lowrez', [5], 10, true);
+    this.animations.add('highrez', [6], 10, true);
+    this.animations.play('highrez');
+
+    cursors = game.input.keyboard.createCursorKeys();
+    cursors.up.onDown.add(jump, this);
+    spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    vkey = game.input.keyboard.addKey(Phaser.Keyboard.V);
+    vkey.onDown.add(vtest, this);
 }
 
-create: function(int x, int y) { 
-    this.player = this.game.add.sprite(x, y, 'dude');
-    this.game.camera.follow(player);
-    
-    this.player.anchor.setTo(.5,.5);
-    this.game.physics.arcade.enable(player);
-    this.player.body.gravity.y  = 500;
-    this.player.body.collideWorldBounds = true;
-    
-    this.player.animations.add('lowrez', [5], 10, true);
-    this.player.animations.add('highrez', [6], 10, true);
-    this.player.animations.play('highrez');
-
-    this.cursors = this.game.input.keyboard.createCursorKeys();
-    this.cursors.up.onDown.add(jump, this);
-    this.spacebar = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-    this.vkey = this.game.input.keyboard.addKey(Phaser.Keyboard.V);
-    this.vkey.onDown.add(vtest, this);
-}
-
-update: function() {
+//function for updating the player's actions. returns whether or not world is shifted.
+player.prototype.update = function() {
     //  Collide the player and the stars with the platforms
-    this.game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(this, platforms);
 
     //  Reset the players velocity if not going too fast (movement)
-    if (Math.abs(this.player.body.velocity.x) <= DEFAULT_SPEED) 
-        this.player.body.velocity.x = 0;
+    if (Math.abs(this.body.velocity.x) <= DEFAULT_SPEED) 
+        this.body.velocity.x = 0;
 
     //  If player is being a smartass do nothing
-    if (this.cursors.right.isDown && this.cursors.left.isDown) {}
+    if (cursors.right.isDown && cursors.left.isDown) {}
     // Handles right arrow input
-    else if (this.cursors.right.isDown) {
+    else if (cursors.right.isDown) {
         //face right
-        if (this.player.scale.x < 0)
-            this.player.scale.x *= -1;
+        if (this.scale.x < 0)
+            this.scale.x *= -1;
         
         //if going too fast to the left, slide to stop. Otherwise, go right
-        if (this.player.body.velocity.x < -1*DEFAULT_SPEED)
-            this.player.body.velocity.x += SIDE_DECAY_NUM;
-        else if (this.player.body.velocity.x <= DEFAULT_SPEED)
-            this.player.body.velocity.x = DEFAULT_SPEED;
+        if (this.body.velocity.x < -1*DEFAULT_SPEED)
+            this.body.velocity.x += SIDE_DECAY_NUM;
+        else if (this.body.velocity.x <= DEFAULT_SPEED)
+            this.body.velocity.x = DEFAULT_SPEED;
     }
     // Handles left arrow input
     else if (this.cursors.left.isDown) {
         //face left
-        if (this.player.scale.x > 0)
-            this.player.scale.x *= -1;
+        if (this.scale.x > 0)
+            this.scale.x *= -1;
         
         //if going too fast to the right, slide to stop. Otherwise, go left
-        if (this.player.body.velocity.x > DEFAULT_SPEED)
-            this.player.body.velocity.x -= SIDE_DECAY_NUM;
-        else if (this.player.body.velocity.x >= -1*DEFAULT_SPEED)
-            this.player.body.velocity.x = -1*DEFAULT_SPEED;
+        if (this.body.velocity.x > DEFAULT_SPEED)
+            this.body.velocity.x -= SIDE_DECAY_NUM;
+        else if (this.body.velocity.x >= -1*DEFAULT_SPEED)
+            this.body.velocity.x = -1*DEFAULT_SPEED;
     }
     
     //  Decays upward momentum if player is not holding up key
-    if (this.cursors.up.isDown && this.player.body.touching.down)
-        this.player.body.velocity.y = JUMP_SPEED;
-    else if (!this.cursors.up.isDown && this.player.body.velocity.y <= UP_DECAY_THRESH) 
+    if (cursors.up.isDown && this.body.touching.down)
+        this.body.velocity.y = JUMP_SPEED;
+    else if (!cursors.up.isDown && this.body.velocity.y <= UP_DECAY_THRESH) 
     {
-        this.player.body.velocity.y *= UP_DECAY_FACTOR;
+        this.body.velocity.y *= UP_DECAY_FACTOR;
     }  
     
     //  Allows player to rapidly descend if airborne
-    if (this.cursors.down.isDown && !this.player.body.touching.down)
+    if (cursors.down.isDown && !this.body.touching.down)
     {
-        if (this.player.body.velocity.y < DOWN_SPEED)
-            this.player.body.velocity.y = DOWN_SPEED;
+        if (this.body.velocity.y < DOWN_SPEED)
+            this.body.velocity.y = DOWN_SPEED;
     }
-
+    
+    return shifted;
 }
 
-bitshift: function() {
+function bitshift() {
     shifted = !shifted;
-    this.player.scale.y *= -1;
+    this.scale.y *= -1;
     if (!shifted) {
-        this.player.animations.play('highrez');
+        this.animations.play('highrez');
     }
     else {
-        this.player.animations.play('lowrez');
+        this.animations.play('lowrez');
     }
 };
 
-vtest: function() {
-    this.player.body.velocity.x += player.scale.x * VTEST_SPEED;
+function vtest() {
+    this.body.velocity.x += this.scale.x * VTEST_SPEED;
 }
 
 }
