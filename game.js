2a3b6350b7da989cfac4d7e735bbad6fd3cc5189
forsooth <maastntes@gmail.com>
2015-10-04 03:46:33 -0400

    Refactoring code. Making programming more object oriented. Adding variables to the objects to which they belong, instead of having global variables everywhere. Adding favicon(s). Adding rocky transition texture (not used yet, it's a-coming).

--- game.js
+++ game.js
@@ -1,58 +1,71 @@
 var gameWidth = 800, gameHeight = 600;
 var game = new Phaser.Game(gameWidth, gameHeight, Phaser.AUTO, "game", {preload:preload, update:update, create:create, render:render});
 
+var fontname = 'Raleway Thin';
 WebFontConfig = {
-    //  The Google Fonts we want to load (specify as many as you like in the array)
-    google: {
-      families: ['Raleway']
-    }
-
+        // The Google Fonts we want to load (specify as many as you like in the array)
+        google: {
+                families: [fontname]
+        }
 };
 
 function preload () {
+        // Level background
         game.load.image('sky', 'assets/sky.png');
         game.load.image('backgrass', 'assets/background_grass_pattern.png');
-        game.load.spritesheet('player', 'assets/protag_spritesheet.png',37, 65);
         game.load.image('hgrass', 'assets/grass_h.png');
         game.load.image('tgrass', 'assets/grass_t.png');
         game.load.image('vgrass', 'assets/grass_v.png');
         game.load.image('darkgrass', 'assets/darkgrass.png');
         game.load.image('floatgrass', 'assets/floating.png');
         game.load.image('floatledge', 'assets/floatingledge.png');
+        // Sprites and stuff 
         game.load.image('boulder', 'assets/boulder.png');
+        game.load.spritesheet('player', 'assets/protag_spritesheet.png',37, 65);
         game.load.spritesheet('turret', 'assets/turretspritesheet.png', 70, 84, 5);
+        // Makes FPS counter work
         game.time.advancedTiming = true;
+        // Allows Google Fonts to be used remotely
         game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 
 }
 
+// The player sprite, extended from Phaser.Sprite
 var player;
+// A group of the static platforms in the level
 var platforms;
-var cursors;
-var spacebar;
-var tilde;
-var vkey;
+// A group containing things that block raycasts
+var obstacles;
+// The controls
+var controls;
+// sprites
 var boulder;
 var turret;
-var shifted = false;
-var PLAYER_X = 200;
-var PLAYER_Y = 500;
-var text;
+// UI text elements
+var respawnText;
+var respawnCount;
 var framerate;
-
+// Time, used for favicon animation
+var then = Date.now();
+// Are we in debug mode?
 var debug_toggle = 0;
 
 function create() {
+        // Create world, with the sky and background grass
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.add.sprite(0, 0, 'sky');
         game.world.setBounds(0, 0, 1920, 600);
         game.add.sprite(0, 300, 'backgrass');
         game.add.sprite(1019, 300, 'backgrass');
+        
+        // Create a new part of the game keeping track of the world resolution
+        game.shifted = false;
 
+        // Create platforms group
         platforms = game.add.group();
         platforms.enableBody = true;
-        tilde = game.input.keyboard.addKey(Phaser.Keyboard.TILDE)
 
+        // Add the "ground" platforms to the bottom of the screen
         for(var i = 0; i < 5; i++) {
                 var ground = platforms.create(252 * i, game.world.height - 64, 'hgrass');
                 var groundshadow = platforms.create(252 * i, game.world.height - 21, 'darkgrass');
@@ -61,72 +74,141 @@ function create() {
                 groundshadow.body.immovable = true;
         }
 
+        // Create the floating rocky grass platforms
         for(var i = 0; i < 2; i++) {
-                var ground = platforms.create(252 * i, game.world.height - 350, 'floatgrass');
-                ground.body.immovable = true;
-                ground.body.setSize(252, 56, 0, 6);
+                var floating = platforms.create(252 * i, game.world.height - 350, 'floatgrass');
+                floating.body.immovable = true;
+                floating.body.setSize(252, 56, 0, 6);
         }
+
+        // Create the ledge that ends the floating rocky grass platforms
         var ledge = platforms.create(504, game.world.height - 350, 'floatledge');
         ledge.body.immovable = true;
         ledge.body.setSize(80, 54, 0, 6);
 
-        boulder = new Boulder(game, 500, 150);
-        turret = new Turret(game, 700, 500);
-        player = new Player(game, PLAYER_X, PLAYER_Y);
-
-        tilde.onDown.add(function() {
+        // Add controls to the game
+        controls = game.input.keyboard.createCursorKeys();
+        controls.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        controls.V = game.input.keyboard.addKey(Phaser.Keyboard.V);
+        controls.tilde = game.input.keyboard.addKey(Phaser.Keyboard.TILDE);
+        // Tilde toggles debug mode
+        controls.tilde.onDown.add(function() {
                 debug_toggle = debug_toggle ? false : true;
+                // If debug is turned off, we want to clear the bounding boxes and make the FPS
+                // counter invisible. This code is put here because in render it would be called
+                // at every frame. 
+                framerate.visible = false;
                 game.debug.reset();
         }, this);
 
-        cursors = game.input.keyboard.createCursorKeys();
-        spacebar = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        spacebar.onDown.add(bitshift, this);
-        vkey = game.input.keyboard.addKey(Phaser.Keyboard.V);
-        vkey.onDown.add(vtest, this);
+        // Create the sprites of the game
+        player = new Player(game, controls);
+        boulder = new Boulder(game, 500, 150);
 
-        text = game.add.text(game.width/2, game.height/2, "");
-        text.fixedToCamera = true;
-        text.anchor.setTo(0.5);
-        text.font = 'Raleway Black';
-        text.fontSize = 120;
-        text.align = 'center';
-        text.fill = '#fff';
-        text.stroke = '#000';
-        text.strokeThickness = 3;
-        text.setShadow(5, 5, 'rgba(0,0,0,0.5)', 5);
-        text.visible = false;
+        obstacles = game.add.group();
+        obstacles.add(platforms);
+        obstacles.add(boulder); // Why does this not work?
+        turret = new Turret(game, player, obstacles, 700, 500);
 
-        framerate = game.add.text(25, 25, game.time.fps);
-        framerate.fixedToCamera = true;
-        framerate.font = "Raleway Light";
-        framerate.fontSize = 14;
-        framerate.fill = '#33ff33';
-        framerate.visible = false;
+        // Set up UI text
+        createUI();
 
 }
 
 function update() {
+        // Collision detection for all objects
         game.physics.arcade.collide(player, platforms);
         game.physics.arcade.collide(platforms, boulder);
         game.physics.arcade.collide(player, boulder);
         game.physics.arcade.collide(player, turret);
         game.physics.arcade.collide(boulder, turret);
 
+        // Update UI
+        if(player.health < 2) {
+                respawnCount.visible = true;
+                respawnText.visible = true;
+                respawnCount.text = Math.ceil((player.RESPAWN_TIME - (Date.now() - player.killTime)) / 1000);
+                if (Date.now() - player.killTime >= player.RESPAWN_TIME) {
+                        respawnCount.visible = false;
+                        respawnText.visible = false;
+                        player.respawn();
+                }
+        }
 
 }
 
 function render() {
-
-        if (debug_toggle == true) {
+        // Don't render anything unless the user wants to debug
+        if (debug_toggle === true) {
+                // Clear the debug render screen, so there isn't any flicker
                 game.debug.context.clearRect(0, 0, game.world.width, game.world.height);
+                // Show and update the framerate
                 framerate.visible = true;
                 framerate.text = game.time.fps;
+                // Draw bounding boxes
                 game.debug.body(player);
                 game.debug.body(boulder);
                 game.debug.body(turret);
                 platforms.forEach(game.debug.body, game.debug, game.debug, 'rgba(255, 30, 30, 0.3)');
-        } else {
-                framerate.visible = false;
+                // Animate the favicon, for fun
+                if(Date.now() - then >= 50) {
+                        then = Date.now();
+                        favicon();
+                }
         }
 }
+
+function createUI() {
+        // Set up text for the large countdown to respawn
+        // We use game.width, not game.world.getBounds().width, because the text is locked to the camera
+        respawnCount = game.add.text(game.width/2, game.height/2, "");
+        // Keep text from moving when player moves
+        respawnCount.fixedToCamera = true;
+        // Set the anchor so centering isn't based off the top left corner
+        respawnCount.anchor.setTo(0.5);
+        // font settings
+        respawnCount.font = fontname;
+        respawnCount.fontSize = 150;
+        respawnCount.align = 'center';
+        respawnCount.fill = '#fff';
+        respawnCount.stroke = '#000';
+        respawnCount.strokeThickness = 3;
+        respawnCount.setShadow(5, 5, 'rgba(0,0,0,1)', 0);
+        // Make the text invisible until a respawn countdown is needed
+        respawnCount.visible = false;
+
+        // Similarly, we need text above the countdown to tell the player they will respawn
+        respawnText = game.add.text(game.width/2, game.height/2 - 150, "Respawn in:");
+        respawnText.fixedToCamera = true;
+        respawnText.anchor.setTo(0.5);
+        respawnText.font = fontname;
+        respawnText.fontSize = 48;
+        respawnText.align = 'center';
+        respawnText.fill = '#fff';
+        respawnText.stroke = '#000';
+        respawnText.strokeThickness = 3;
+        respawnText.setShadow(3, 3, 'rgba(0,0,0,1)', 0);
+        respawnText.visible = false;
+
+        // Add an FPS counter to the top left corner, to be turned on with debug mode
+        framerate = game.add.text(25, 25, game.time.fps);
+        framerate.fixedToCamera = true;
+        framerate.font = fontname;
+        framerate.fontSize = 14;
+        framerate.fill = '#33ff33';
+        framerate.stroke = '#000';
+        framerate.strokeThickness = 3;
+        framerate.visible = false;
+}
+
+/*
+function bitshift() {
+    shifted = !shifted;
+    player.scale.y *= -1;
+    if (!shifted) {
+//        this.animations.play('highrez');
+    }
+    else {
+//        this.animations.play('lowrez');
+    }
+}*/
--- player.js
+++ player.js
@@ -1,181 +1,203 @@
-//js
-//contains code for player actions and sprite
-//requires preloading: this.game.load.spritesheet('player','assets/dude.png',32,48);
+function Player(game, controls) {
+        // Store variables
+        this.game = game;
+        this.controls = controls;
+        this.facingRight = true;
+        this.killTime = 0;
+        this.dying = false;
+    
+        //Store constants
+        // Player initial coordinates
+        this.INIT_X = 200;
+        this.INIT_Y = 500;
+        // Walking and jumping speeds
+        this.DEFAULT_SPEED = 150;
+        this.JUMP_SPEED = -450;
+        // Constants to make jumps higher if the key is held down
+        this.UP_DECAY_FACTOR = 0.8;
+        this.UP_DECAY_THRESH = -100;
+        // Decay factor for coming down from a speedboost
+        this.SIDE_DECAY_NUM = 5;
+        // Speed for the accelerated downward movement (ground pound)
+        this.DOWN_SPEED = 400;
+        // Speedboost speed
+        this.V_SPEED = 30;
+        // Respawn time, in milliseconds
+        this.RESPAWN_TIME = 4000;
+        // Player health. We only have one-hit deaths, but setting
+        // health to zero triggers some default Phaser code, so we
+        // use 1 and 2 as our values for alive and dead.
+        this.MAX_HEALTH = 2;
+
+
+        // Initialize the player sprite
+        Phaser.Sprite.call(this, game, this.INIT_X, this.INIT_Y, 'player');
+        this.anchor.setTo(0.5, 0.5);
+        game.physics.enable(this, Phaser.Physics.ARCADE);
+        game.add.existing(this);
+        // Add gravity and don't let the player pass offscreen
+        this.body.gravity.y = 500;
+        this.body.collideWorldBounds = true;
+        // Have the game camera always centered on the player
+        game.camera.follow(this);
+        // Initialize alive-ness
+        this.health = this.MAX_HEALTH;
+    
+        //  Name animations for the player
+        // walking
+        //                   name,     frames from sheet,    FPS, loop
+        this.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
+        this.animations.add('right', [10, 11, 12, 13, 14, 15, 16, 17], 10, true);
+        // jumping
+        this.animations.add('jright', [18, 19, 20], 4, false);
+        this.animations.add('jleft', [21, 22, 23], 4, false);
+
+}
 
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
-var DEFAULT_SPEED = 150;
-var JUMP_SPEED = -450;
-var UP_DECAY_FACTOR = .8;
-var UP_DECAY_THRESH = -100;
-var SIDE_DECAY_NUM = 5;
-var DOWN_SPEED = 400;
-var VTEST_SPEED = 300;
-var RESPAWN_TIME = 4000; //milliseconds
-var shifted = false;
-var faceright = true;
-var timecheck;
-
-//Creates a player at x and y
-function Player(game, x, y) {
-    Phaser.Sprite.call(this, game, x, y, 'player');
-    this.anchor.setTo(.5,.5);
-    game.physics.arcade.enable(this);
-
-    game.add.existing(this);
-    this.body.gravity.y  = 500;
-    this.body.collideWorldBounds = true;
-    game.camera.follow(this);
-
-    this.health = 2;
-    
-    //  adding animations
-    this.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7], 10, true);
-    this.animations.add('right', [10, 11, 12, 13, 14, 15, 16, 17], 10, true);
-    this.animations.add('jright', [18, 19, 20], 4, false);
-    this.animations.add('jleft', [21, 22, 23], 4, false);
-
-//    this.animations.add('lowrez', [5], 10, true);
-//    this.animations.add('highrez', [6], 10, true);
-//    this.animations.play('highrez');
-
-}
-
-//function for updating the player's actions. returns whether or not world is shifted.
 Player.prototype.update = function() {
-    if(this.health < 2) {
-        text.visible = true;
-        text.text = Math.round((RESPAWN_TIME - (Date.now() - timecheck)) / 1000);
-        if (Date.now() - timecheck >= RESPAWN_TIME) {
-            text.visible = false;
-            this.respawn();
+        // If the player is dead and waiting to be respawned, don't let the user
+        // move the player around.
+        if (this.health < 2) {
+                this.animations.currentAnim.stop();
+                this.body.velocity.x = 0;
+                this.body.velocity.y = 0;
+                return;
         }
 
-        return;
-    }
-
-    //  Reset the players velocity if not going too fast (movement)
-    if (Math.abs(this.body.velocity.x) <= DEFAULT_SPEED) {
-        this.body.velocity.x = 0;
-    }
-    if (this.body.touching.down) {
-        this.body.velocity.y = 0;
-    }
-    
-    //  If player is being a smartass do nothing
-    if (cursors.right.isDown && cursors.left.isDown) {
-        if(player.animations.currentAnim === player.animations.getAnimation('jleft')){
-            this.body.velocity.x = DEFAULT_SPEED;
-            player.frame = 23;
+        // Reset velocities if touching the ground or moving slowly
+        if (Math.abs(this.body.velocity.x) <= this.DEFAULT_SPEED) {
+                this.body.velocity.x = 0;
         }
-        else if(player.animations.currentAnim === player.animations.getAnimation('jright')){
-            this.body.velocity.x = -1*DEFAULT_SPEED;
-            player.frame = 20;
+        if (this.body.touching.down || this.body.blocked.down) {
+                this.body.velocity.y = 0;
         }
-        else
-            player.animations.stop();
-            player.frame = 9;
-    }
-    // Handles right arrow input
-    else if (cursors.right.isDown) {
-        faceright = true;
-        if(this.body.touching.down)
-            player.animations.play('right');
         
-        //if going too fast to the left, slide to stop. Otherwise, go right
-        if (this.body.velocity.x < -1*DEFAULT_SPEED)
-            this.body.velocity.x += SIDE_DECAY_NUM;
-        else if (this.body.velocity.x <= DEFAULT_SPEED)
-            this.body.velocity.x = DEFAULT_SPEED;
-    }
-    // Handles left arrow input
-    else if (cursors.left.isDown) {
-        faceright = false;
-        if(this.body.touching.down)
-            player.animations.play('left');
-        /*else if (!this.body.touching.down && this.animations.currentAnim != null)
-            player.animation.play('jleft')*/
+
+        // CONTROLS
+
+        //  If player pushing down both arrow keys, don't move
+        if ((this.controls.right.isDown && this.controls.left.isDown) || (this.controls.up.isDown && this.controls.down.isDown)) {
+                this.body.velocity.x = 0;
+                this.body.velocity.y = 0;
+                this.animations.stop();
+                // Frame for idling
+                this.frame = 9;
+        }
+
+        // Right Arrow
+        else if (this.controls.right.isDown) {
+                this.facingRight = true;
+                // If we are touching the ground, walk to the right
+                if(this.body.touching.down || this.body.blocked.down) {
+                        this.animations.play('right');
+                }
+            
+                // This is rightwards motion, so if we are moving to the left with a high
+                // velocity, decay that speed until we are back at the default. Otherwise,
+                // it is business as usual and we go to the right at our default speed.
+                if (this.body.velocity.x < -1 * this.DEFAULT_SPEED) {
+                        this.body.velocity.x += this.SIDE_DECAY_NUM;
+                } else if (this.body.velocity.x <= this.DEFAULT_SPEED) {
+                        this.body.velocity.x = this.DEFAULT_SPEED;
+                }
+        }
+
+        // Left Arrow
+        else if (this.controls.left.isDown) {
+                this.facingRight = false;
+                // If we are touching the ground, walk to the left
+                if(this.body.touching.down || this.body.blocked.down) {
+                        this.animations.play('left');
+                }
+            
+                // Same as above, if we are going fast to the right, the left arrow key should
+                // slow us down. If not, then just change our speed to the default walking speed.
+                if (this.body.velocity.x > this.DEFAULT_SPEED) {
+                        this.body.velocity.x -= this.SIDE_DECAY_NUM;
+                } else if (this.body.velocity.x >= -1 * this.DEFAULT_SPEED) {
+                        this.body.velocity.x = -1 * this.DEFAULT_SPEED;
+                }
+        }
+    
+        // Reset the animation when the player is idle, so we don't walk in place
+        if (!this.controls.left.isDown && !this.controls.right.isDown && (this.body.touching.down || this.body.blocked.down)) {
+                if(this.facingRight){
+                        this.frame = 9;
+                } else {
+                        this.frame = 8;
+                }
+        }
         
-        //if going too fast to the right, slide to stop. Otherwise, go left
-        if (this.body.velocity.x > DEFAULT_SPEED)
-            this.body.velocity.x -= SIDE_DECAY_NUM;
-        else if (this.body.velocity.x >= -1*DEFAULT_SPEED)
-            this.body.velocity.x = -1*DEFAULT_SPEED;
-    }
-
-    // so player doesn't keep animating when idle
-    if (!cursors.left.isDown && !cursors.right.isDown && this.body.touching.down && this.animations.currentAnim != null) {
-        if(!faceright){
-            player.frame = 8;
+        // Up Arrow
+        // If the up arrow is being pushed, give the player a velocity upwards as long as
+        // the player is touching the ground.
+        if (this.controls.up.isDown && (this.body.touching.down || this.body.blocked.down)) {
+                this.body.velocity.y = this.JUMP_SPEED;
+                // Play jump animations
+                if (this.facingRight){
+                        this.animations.play('jright');
+                } else {
+                        this.animations.play('jleft');
+                }
         }
-        else
-            player.frame = 9;
-
-    }
-    
-    //  Decays upward momentum if player is not holding up key
-    if (cursors.up.isDown && (this.body.touching.down || this.body.blocked.down)) {
-        this.body.velocity.y = JUMP_SPEED;
-
-    // plays jump animations
-        if (!faceright){
-            player.animations.play('jleft');
-        }
-        else 
-            player.animations.play('jright');
-    }
-
-    else if (!cursors.up.isDown && this.body.velocity.y <= UP_DECAY_THRESH) 
-    {
-        this.body.velocity.y *= UP_DECAY_FACTOR;
-    } 
-    
-    //  Allows player to rapidly descend if airborne
-    if (cursors.down.isDown && !this.body.touching.down)
-    {
-        if (this.body.velocity.y < DOWN_SPEED){
-            this.body.velocity.y = DOWN_SPEED;
-            if(!faceright){
-                player.frame = 25;
-            }
-            else
-                player.frame = 24;
+        // If the user is not holding down the up key, we make the jump decay faster than
+        // it normally would. This makes it so holding down the up arrow leads to a 
+        // higher jump.
+        else if (!this.controls.up.isDown && this.body.velocity.y <= this.UP_DECAY_THRESH) {
+                this.body.velocity.y *= this.UP_DECAY_FACTOR;
+        } 
+        
+        // Down Arrow
+        //  Allows player to descend rapidly if airborne
+        if (this.controls.down.isDown && !(this.body.touching.down || this.body.blocked.down)) {
+                // If we are falling already, set our speed to be the down speed and let
+                // gravity add to that as we fall. It is important that this key never makes
+                // the player slower, so triggering it when at a low falling speed will
+                // boost the player speed, but at the next frame this conditional will be
+                // false and so gravity will increase the player's speed.
+                if (this.body.velocity.y < this.DOWN_SPEED){
+                        this.body.velocity.y = this.DOWN_SPEED;
+                        // Set the falling animations
+                        if(this.facingRight){
+                                this.frame = 24;
+                        }
+                        else {
+                                this.frame = 25;
+                        }
+                }
         }
 
-        if (this.body.touching.down) {
-            this.body.velocity.y = 0;
+        // V Key (speedboost)
+        if (this.controls.V.isDown) {
+                // Add the speedboost speed to the player's velocity.
+                // The weird math makes it so if the player is facing
+                // right the speed added will be 1 * V_SPEED as 2 * 1 - 1 = 1,
+                // whereas if the player is facing left 2 * 0 - 1 = -1, 
+                // and a velocity of -V_SPEED will be added.  
+                this.body.velocity.x += ((2 * this.facingRight) - 1) * this.V_SPEED;
         }
-    }
-}
+};
 
-function bitshift() {
-    shifted = !shifted;
-    player.scale.y *= -1;
-    if (!shifted) {
-//        this.animations.play('highrez');
-    }
-    else {
-//        this.animations.play('lowrez');
-    }
-}
-
-function vtest(p) {
-    player.body.velocity.x += (2*faceright-1) * VTEST_SPEED;
-}
-
+// Function to kill the player, and begin the respawn cycle.
 Player.prototype.kill = function() {
-        this.health -= 1;
-        timecheck = Date.now();
+        // Set health to 'dead'
+        this.health = this.MAX_HEALTH - 1;
+        // Mark the time of death, and reset all player variables
+        // so the player corpse doesn't zombie around the screen
+        this.killTime = Date.now();
         this.body.velocity.x = 0;
         this.body.velocity.y = 0;
         this.body.gravity.y = 0;
-}
+};
 
+// Reset the player
 Player.prototype.respawn = function() {
-        shifted = false;
-        this.health = 2;
+        this.game.shifted = false;
+        this.health = this.MAX_HEALTH;
         this.body.gravity.y = 500;
-        this.position.x = PLAYER_X;
-        this.position.y = PLAYER_Y;
-}
+        this.position.x = this.INIT_X;
+        this.position.y = this.INIT_Y;
+};
