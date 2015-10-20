8b6a9af015e8f5cdbb91e2dd72ce8a9baa818b6c
FuryLordofDerp <furykat@gmail.com>
2015-10-20 03:56:31 -0400

    Pushing current iteration of win. Broken.

--- js/win.js
+++ js/win.js
@@ -4,6 +4,8 @@ var winState = {
 		// load menu images
 		game.load.spritesheet('dialogue', 'assets/ending.png', 257, 165, 18);
 		game.load.spritesheet('player', 'assets/art/default/protag_spritesheet.png', 37, 65);
+		game.load.spritesheet('segfault', 'assets/jumpscare_spritesheet.png', 800, 600, 6);
+		game.load.audio('scream', ['assets/music/default/scream.wav', 'assets/music/scream.ogg']);
 		//game.load.spritesheet('sky', 'assets/art/ice/ice_bg_spritesheet.png', 1920, 1080, 2);
 		//game.load.spritesheet('hgrass', 'assets/art/ice/ice_h_spritesheet.png', 252, 48, 2);
 
@@ -18,6 +20,13 @@ var winState = {
 		frame = 0;
 		frames = 11;
 		shifted = false;
+
+		// get ready to be spooked
+		segfault = game.add.sprite(0,0,'segfault');
+		segfault.animations.add('spooked', [0,3,5,1,3,4,2,3,0,1,2,3,4,5], 11, false);
+		segfault.visible = false;
+		this.scream = game.add.audio('scream');
+        this.scream.volume = 0.5;
 
 		// creates menu controls
 		controls = game.input.keyboard.createCursorKeys();
@@ -58,6 +67,9 @@ var winState = {
 			frame++;
 			// if on the last dialogue box and user advances
 			if(frame === (frames-1)){
+				this.scream.play();
+				segfault.visible = true;
+				//segfault.animations.play('spooked');
 				game.state.start('menu');
 			}
 		}
